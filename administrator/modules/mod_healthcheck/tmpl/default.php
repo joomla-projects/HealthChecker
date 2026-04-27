cb660df10d71491a6fdffff90e29556e8b2268d8
Olivier <olivier.buisard@simplifyyourweb.com>
2026-04-27 17:14:35 -0400

    Icon refresh should be small

--- administrator/modules/mod_healthcheck/tmpl/default.php
+++ administrator/modules/mod_healthcheck/tmpl/default.php
@@ -51,7 +51,7 @@ $has_data = !empty($gauges_html) || !empty($buttons_html) || !empty($lists_html)
                 <?php echo Text::_('MOD_HEALTHCHECK_FILTER_CRITICAL'); ?>
             </button>
         </div>
-        <button type="button" class="btn btn-primary">
+        <button type="button" class="btn btn-sm btn-primary">
             <i class="icon-refresh"></i>
         </button>
     </div>
